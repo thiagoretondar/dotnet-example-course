13e0ab7bc72c4699a69f6d510ce599bb03a47407
Thiago Retondar <t.retondar@gmail.com>
2017-11-18 16:50:06 -0200

    created features endpoint

---
+++ Migrations/20171118184705_SeedFeatures.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                });
+
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature1')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature2')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+        }
+    }
+}
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -5,11 +5,12 @@ namespace vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
         {
         }
-
-        public DbSet<Make> Makes { get; set; }
     }
 }
